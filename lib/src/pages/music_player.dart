26b49f5ccca2b7a970afa7a8bec982866a864568
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-05-25 18:54:38 -0500

    Desing of custom appbar

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/src/widgets/custom_appbar.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
+      body: Column(
+        children: [CustomAppBar()],
       ),
     );
   }
