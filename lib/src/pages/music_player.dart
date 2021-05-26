be3885ecd66775e1f04449232c449d57cf1d4369
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-05-25 21:42:52 -0500

    Adding disk image

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -6,7 +6,65 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [CustomAppBar()],
+        children: [CustomAppBar(), ImagenDiscoDuracion()],
+      ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  const ImagenDiscoDuracion({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [ImagenDisco()],
+      ),
+    );
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  const ImagenDisco({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(200),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image(image: AssetImage('assets/aurora.jpg')),
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(100)),
+              ),
+              Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                    color: Color(0xff1C1C25),
+                    borderRadius: BorderRadius.circular(100)),
+              )
+            ],
+          )),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            colors: [Color(0xff484750), Color(0xff1E1C24)]),
       ),
     );
   }
