390e64bdcb0db16903fa081293dd7111709606b0
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-05-25 21:49:33 -0500

    Adding progress bar design

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -20,8 +20,59 @@ class ImagenDiscoDuracion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      margin: EdgeInsets.only(top: 70),
       child: Row(
-        children: [ImagenDisco()],
+        children: [
+          ImagenDisco(),
+          SizedBox(width: 20),
+          BarraProgreso(),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgreso extends StatelessWidget {
+  const BarraProgreso({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(color: Colors.white.withOpacity(0.4));
+    return Container(
+      child: Column(
+        children: [
+          Text('00:00', style: estilo),
+          const SizedBox(
+            height: 10,
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.1),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 3,
+                  height: 150,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text('00:00', style: estilo)
+        ],
       ),
     );
   }
