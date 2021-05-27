3d897f3bdc192203d5292f7015b2d229949b84ef
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-05-26 21:36:34 -0500

    Adding background gradient

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -6,13 +6,43 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CustomAppBar(),
-          const ImagenDiscoDuracion(),
-          const TituloPlay(),
-          const Expanded(child: Lyrics())
-        ],
+      body: Stack(children: [
+        Background(),
+        Column(
+          children: [
+            CustomAppBar(),
+            const ImagenDiscoDuracion(),
+            const TituloPlay(),
+            const Expanded(child: Lyrics())
+          ],
+        ),
+      ]),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.8,
+      //color: Colors.red,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+        gradient: LinearGradient(
+          end: Alignment.center,
+          colors: [
+            Color(0xff33333E),
+            Color(0xff201E28),
+          ],
+        ),
       ),
     );
   }
